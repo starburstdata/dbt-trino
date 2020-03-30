95ea48045de6ed6b9eadda6fa733622f5186fcac
Jacob Beck <jake@fishtownanalytics.com>
2020-03-30 13:15:03 -0600

    Fix the tests
    fix cirleci errors
    flake8
    pushed a presto image with our configs
      use that in docker-compose
    add flake8/pytest

--- dbt/adapters/presto/__init__.py
+++ dbt/adapters/presto/__init__.py
@@ -1,6 +1,6 @@
-from dbt.adapters.presto.connections import PrestoConnectionManager
+from dbt.adapters.presto.connections import PrestoConnectionManager  # noqa
 from dbt.adapters.presto.connections import PrestoCredentials
-from dbt.adapters.presto.column import PrestoColumn
+from dbt.adapters.presto.column import PrestoColumn  # noqa
 from dbt.adapters.presto.impl import PrestoAdapter
 
 from dbt.adapters.base import AdapterPlugin
