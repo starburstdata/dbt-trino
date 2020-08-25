ee4db21f2d09fb75e31d01c0476095107f5a2be7
Jacob Beck <jake@fishtownanalytics.com>
2020-08-25 09:44:10 -0600

    archives -> snapshots, fix drop_schema

--- dbt/include/presto/macros/materializations/archive.sql
+++
@@ -1,6 +0,0 @@
-
-{% materialization archive, adapter='presto' -%}
-  {{ exceptions.raise_not_implemented(
-    'archive materialization not implemented for '+adapter.type())
-  }}
-{% endmaterialization %}
