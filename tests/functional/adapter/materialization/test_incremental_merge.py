a78a4f153c2dce3e0b1e58b9f6e221f3ecf6e12e
Przemek Denkiewicz <przemyslaw.denkiewicz@starburstdata.com>
2022-08-22 13:13:43 +0200

    Skip merge tests for Starburst

--- tests/functional/adapter/materialization/test_incremental_merge.py
+++ tests/functional/adapter/materialization/test_incremental_merge.py
@@ -14,6 +14,8 @@ from dbt.tests.adapter.incremental.test_incremental_unique_id import (
     seeds__seed_csv,
 )
 
+from tests.conftest import get_engine_type
+
 seeds__duplicate_insert_sql = """
 -- Insert statement which when applied to seed.csv triggers the inplace
 --   overwrite strategy of incremental models. Seed and incremental model
@@ -124,6 +126,9 @@ class TrinoIncrementalUniqueKey(BaseIncrementalUniqueKey):
 
 
 @pytest.mark.iceberg
+@pytest.mark.skipif(
+    condition=get_engine_type() == "starburst", reason="Starburst doesn't support MERGE yet"
+)
 class TestIcebergIncrementalMerge(TrinoIncrementalUniqueKey):
     @pytest.fixture(scope="class")
     def project_config_update(self):
@@ -135,6 +140,9 @@ class TestIcebergIncrementalMerge(TrinoIncrementalUniqueKey):
 
 
 @pytest.mark.delta
+@pytest.mark.skipif(
+    condition=get_engine_type() == "starburst", reason="Starburst doesn't support MERGE yet"
+)
 class TestDeltaIncrementalMerge(TrinoIncrementalUniqueKey):
     @pytest.fixture(scope="class")
     def project_config_update(self):
