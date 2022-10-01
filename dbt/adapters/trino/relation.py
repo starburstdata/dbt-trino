15520b398508051c2b744a114982f8b78d333dd3
NGUYEN HUU BACH <bachng@gmail.com>
2022-10-01 16:46:16 +0000

    change relation matching to case-insensitive

--- dbt/adapters/trino/relation.py
+++ dbt/adapters/trino/relation.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 
 from dbt.adapters.base.relation import BaseRelation, Policy
+from dbt.contracts.relation import ComponentName
 
 
 @dataclass
@@ -13,3 +14,7 @@ class TrinoQuotePolicy(Policy):
 @dataclass(frozen=True, eq=False, repr=False)
 class TrinoRelation(BaseRelation):
     quote_policy: TrinoQuotePolicy = TrinoQuotePolicy()
+
+    # Overridden as Trino converts relation identifiers to lowercase
+    def _is_exactish_match(self, field: ComponentName, value: str) -> bool:
+        return self.path.get_lowered_part(field) == value.lower()
