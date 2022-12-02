d48f018c282f7b93a8919857f8c603db335d78dc
Damian Owsianny <damian.owsianny@starburstdata.com>
2022-12-02 10:55:41 +0100

    Fix get_columns_in_relation timezone type detection

--- dbt/adapters/trino/column.py
+++ dbt/adapters/trino/column.py
@@ -1,7 +1,9 @@
+import re
 from dataclasses import dataclass
 from typing import ClassVar, Dict
 
 from dbt.adapters.base.column import Column
+from dbt.exceptions import RuntimeException
 
 
 @dataclass
@@ -22,4 +24,45 @@ class TrinoColumn(Column):
         # some of the Trino data types specify a type and not a precision
         if raw_data_type.startswith(("array", "map", "row")):
             return cls(name, raw_data_type)
-        return super(TrinoColumn, cls).from_description(name, raw_data_type)
+        match = re.match(
+            r"(?P<type>[^(]+)(?P<size>\([^)]+\))?(?P<type_suffix>[\w ]+)?", raw_data_type
+        )
+        if match is None:
+            raise RuntimeException(f'Could not interpret data type "{raw_data_type}"')
+        data_type = match.group("type")
+        size_info = match.group("size")
+        data_type_suffix = match.group("type_suffix")
+        if data_type_suffix:
+            data_type += data_type_suffix
+        char_size = None
+        numeric_precision = None
+        numeric_scale = None
+        if size_info is not None:
+            # strip out the parentheses
+            size_info = size_info[1:-1]
+            parts = size_info.split(",")
+            if len(parts) == 1:
+                try:
+                    char_size = int(parts[0])
+                except ValueError:
+                    raise RuntimeException(
+                        f'Could not interpret data_type "{raw_data_type}": '
+                        f'could not convert "{parts[0]}" to an integer'
+                    )
+            elif len(parts) == 2:
+                try:
+                    numeric_precision = int(parts[0])
+                except ValueError:
+                    raise RuntimeException(
+                        f'Could not interpret data_type "{raw_data_type}": '
+                        f'could not convert "{parts[0]}" to an integer'
+                    )
+                try:
+                    numeric_scale = int(parts[1])
+                except ValueError:
+                    raise RuntimeException(
+                        f'Could not interpret data_type "{raw_data_type}": '
+                        f'could not convert "{parts[1]}" to an integer'
+                    )
+
+        return cls(name, data_type, char_size, numeric_precision, numeric_scale)
