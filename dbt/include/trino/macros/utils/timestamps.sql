d6dbbcf8583582af621901193c46883aa5a6d331
Robbert Hofman <117672004+RobbertDM@users.noreply.github.com>
2022-12-29 14:44:56 +0100

    Update timestamps.sql

--- dbt/include/trino/macros/utils/timestamps.sql
+++ dbt/include/trino/macros/utils/timestamps.sql
@@ -1,5 +1,5 @@
 {% macro trino__current_timestamp() -%}
-    current_timestamp
+    cast(current_timestamp as timestamp(6) with time zone)
 {%- endmacro %}
 
 {% macro trino__snapshot_string_as_time(timestamp) %}
