032fd6e460749dfd4f27af4c5e829515d76647bb
Michiel De Smet <michiel.desmet@starburstdata.com>
2023-02-14 22:05:26 +0100

    Change snapshot to IS DISTINCT FROM

--- dbt/include/trino/macros/materializations/snapshot.sql
+++ dbt/include/trino/macros/materializations/snapshot.sql
@@ -32,3 +32,37 @@
             {%- endfor %})
 
 {% endmacro %}
+
+{% macro snapshot_check_strategy(node, snapshotted_rel, current_rel, config, target_exists) %}
+    {% set check_cols_config = config['check_cols'] %}
+    {% set primary_key = config['unique_key'] %}
+    {% set invalidate_hard_deletes = config.get('invalidate_hard_deletes', false) %}
+    {% set updated_at = config.get('updated_at', snapshot_get_time()) %}
+
+    {% set column_added = false %}
+
+    {% set column_added, check_cols = snapshot_check_all_get_existing_columns(node, target_exists, check_cols_config) %}
+
+    {%- set row_changed_expr -%}
+    (
+    {%- if column_added -%}
+        {{ get_true_sql() }}
+    {%- else -%}
+    {%- for col in check_cols -%}
+        {{ snapshotted_rel }}.{{ col }} IS DISTINCT FROM {{ current_rel }}.{{ col }}
+        {%- if not loop.last %} or {% endif -%}
+    {%- endfor -%}
+    {%- endif -%}
+    )
+    {%- endset %}
+
+    {% set scd_id_expr = snapshot_hash_arguments([primary_key, updated_at]) %}
+
+    {% do return({
+        "unique_key": primary_key,
+        "updated_at": updated_at,
+        "row_changed": row_changed_expr,
+        "scd_id": scd_id_expr,
+        "invalidate_hard_deletes": invalidate_hard_deletes
+    }) %}
+{% endmacro %}
