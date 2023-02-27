b610bd864918a4c5790a7bf524ce131290731f2e
Damian Owsianny <damian.owsianny@starburstdata.com>
2023-02-27 12:28:01 +0100

    Adjust 1.1.latest branch to backport convention

--- setup.py
+++ setup.py
@@ -11,30 +11,38 @@ with open(os.path.join(this_directory, "README.md")) as f:
 package_name = "dbt-trino"
 
 
-# get this from a separate file
-def _dbt_trino_version():
-    _version_path = os.path.join(
-        this_directory, "dbt", "adapters", "trino", "__version__.py"
-    )
-    _version_pattern = r"""version\s*=\s*["'](.+)["']"""
+# get this package's version from dbt/adapters/<name>/__version__.py
+def _get_plugin_version_dict():
+    _version_path = os.path.join(this_directory, "dbt", "adapters", "trino", "__version__.py")
+    _semver = r"""(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)"""
+    _pre = r"""((?P<prekind>a|b|rc)(?P<pre>\d+))?"""
+    _version_pattern = rf"""version\s*=\s*["']{_semver}{_pre}["']"""
     with open(_version_path) as f:
         match = re.search(_version_pattern, f.read().strip())
         if match is None:
             raise ValueError(f"invalid version at {_version_path}")
-        return match.group(1)
+        return match.groupdict()
+
+
+def _dbt_trino_version():
+    parts = _get_plugin_version_dict()
+    trino_version = "{major}.{minor}.{patch}".format(**parts)
+    if parts["prekind"] and parts["pre"]:
+        trino_version += parts["prekind"] + parts["pre"]
+    return trino_version
+
+
+# require a compatible minor version (~=), prerelease if this is a prerelease
+def _get_dbt_core_version():
+    parts = _get_plugin_version_dict()
+    minor = "{major}.{minor}.0".format(**parts)
+    pre = parts["prekind"] + "1" if parts["prekind"] else ""
+    return f"{minor}{pre}"
 
 
 package_version = _dbt_trino_version()
 description = """The trino adapter plugin for dbt (data build tool)"""
-
-dbt_version = "1.1.1"
-# the package version should be the dbt version, with maybe some things on the
-# ends of it. (0.19.1 vs 0.19.1a1, 0.19.1.1, ...)
-if not package_version.startswith(dbt_version):
-    raise ValueError(
-        f"Invalid setup.py: package_version={package_version} must start with "
-        f"dbt_version={dbt_version}"
-    )
+dbt_version = _get_dbt_core_version()
 
 setup(
     name=package_name,
