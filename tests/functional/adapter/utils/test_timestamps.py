1a6694d146fc7c0cf13f2f142cd9cc8620bd9427
Robbert Hofman <robbert.hofman@dataminded.be>
2023-02-28 13:51:20 +0100

    add test for timestamps

--- tests/functional/adapter/utils/test_timestamps.py
+++ tests/functional/adapter/utils/test_timestamps.py
@@ -16,3 +16,19 @@ class TestCurrentTimestampTrino(BaseCurrentTimestamps):
     @pytest.fixture(scope="class")
     def expected_sql(self):
         return 'select current_timestamp as "current_timestamp"'
+
+
+class TestCurrentTimestamp6Trino(BaseCurrentTimestamps):
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "get_current_timestamp_6.sql": 'select {{ current_timestamp(6) }} as "current_timestamp"'
+        }
+
+    @pytest.fixture(scope="class")
+    def expected_schema(self):
+        return {"current_timestamp": "timestamp(6) with time zone"}
+
+    @pytest.fixture(scope="class")
+    def expected_sql(self):
+        return 'select current_timestamp(6) as "current_timestamp"'
