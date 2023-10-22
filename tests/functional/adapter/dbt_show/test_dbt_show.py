f34d46f2a026c95bf6a7e38554cb02de002e00fd
Damian Owsianny <damian3031@gmail.com>
2023-10-22 19:31:43 +0200

    Add test_dbt_show test

---
+++ tests/functional/adapter/dbt_show/test_dbt_show.py
@@ -0,0 +1,9 @@
+from dbt.tests.adapter.dbt_show.test_dbt_show import BaseShowLimit, BaseShowSqlHeader
+
+
+class TestTrinoShowSqlHeader(BaseShowSqlHeader):
+    pass
+
+
+class TestTrinoShowLimit(BaseShowLimit):
+    pass
