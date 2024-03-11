772bb6905af4b9bbdfd427639ec509b696374f58
Damian Owsianny <damian3031@gmail.com>
2024-03-11 21:11:53 +0100

    Fix tests for ephemeral materialization

--- tests/functional/adapter/test_ephemeral.py
+++ tests/functional/adapter/test_ephemeral.py
@@ -1,12 +1,26 @@
-from dbt.tests.adapter.ephemeral.test_ephemeral import BaseEphemeralMulti
+from dbt.tests.adapter.ephemeral.test_ephemeral import (
+    BaseEphemeralErrorHandling,
+    BaseEphemeralMulti,
+    BaseEphemeralNested,
+)
 from dbt.tests.util import check_relations_equal, run_dbt
 
 
 class TestEphemeralMultiTrino(BaseEphemeralMulti):
-    def test_ephemeral_multi_trino(self, project):
+    def test_ephemeral_multi(self, project):
         run_dbt(["seed"])
         results = run_dbt(["run"])
         assert len(results) == 3
         check_relations_equal(
             project.adapter, ["SEED", "DEPENDENT", "DOUBLE_DEPENDENT", "SUPER_DEPENDENT"]
         )
+
+
+class TestEphemeralNestedTrino(BaseEphemeralNested):
+    def test_ephemeral_nested(self, project):
+        results = run_dbt(["run"])
+        assert len(results) == 2
+
+
+class TestEphemeralErrorHandlingTrino(BaseEphemeralErrorHandling):
+    pass
