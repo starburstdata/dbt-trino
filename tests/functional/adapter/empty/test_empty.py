40df218d6f889797e0b25ed2dabcd4c5db1e1f99
Damian Owsianny <damian3031@gmail.com>
2024-05-06 13:08:18 +0200

    Stop adding aliases to render_limited output

--- tests/functional/adapter/empty/test_empty.py
+++ tests/functional/adapter/empty/test_empty.py
@@ -1,5 +1,12 @@
-from dbt.tests.adapter.empty.test_empty import BaseTestEmpty
+from dbt.tests.adapter.empty.test_empty import (
+    BaseTestEmpty,
+    BaseTestEmptyInlineSourceRef,
+)
 
 
 class TestTrinoEmpty(BaseTestEmpty):
+    pass
+
+
+class TestTrinoEmptyInlineSourceRef(BaseTestEmptyInlineSourceRef):
     pass
