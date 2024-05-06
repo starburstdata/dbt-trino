3f00b292c1bdf6e8a7e1817bc50da854505dc840
Damian Owsianny <damian3031@gmail.com>
2024-05-06 13:08:18 +0200

    Stop adding aliases to render_limited output

--- dbt/adapters/trino/relation.py
+++ dbt/adapters/trino/relation.py
@@ -7,6 +7,7 @@ from dbt.adapters.contracts.relation import ComponentName
 @dataclass(frozen=True, eq=False, repr=False)
 class TrinoRelation(BaseRelation):
     quote_policy: Policy = field(default_factory=lambda: Policy())
+    require_alias: bool = False
 
     # Overridden as Trino converts relation identifiers to lowercase
     def _is_exactish_match(self, field: ComponentName, value: str) -> bool:
